e773ed02e6b406c12c7d078de53afa44ca0bec47
⧉⧉⧉ <mm@shotnoise>
2023-08-23 18:31:06 +0200

    Fix example: grovers_search

--- examples/grovers_search.rs
+++ examples/grovers_search.rs
@@ -4,92 +4,93 @@
 //! Implements Grover's algorithm for unstructured search,
 //! using only X, H and multi-controlled Z gates.
 
-// use quest_bind::{
-//     Qreal,
-//     QuestEnv,
-//     QuestError,
-//     Qureg,
-//     PI,
-// };
-// use rand::Rng;
+use quest_bind::{
+    Qreal,
+    QuestEnv,
+    QuestError,
+    Qureg,
+    PI,
+};
+use rand::Rng;
 
-// const NUM_QUBITS: i32 = 0x10;
-// const NUM_ELEMS: i64 = 1 << NUM_QUBITS;
+const NUM_QUBITS: i32 = 0x10;
+const NUM_ELEMS: i64 = 1 << NUM_QUBITS;
 
-// fn tensor_gate<F, const N: u16>(
-//     qureg: &mut Qureg<'_, N>,
-//     gate: F,
-//     qubits: &[i32],
-// ) -> Result<(), QuestError>
-// where
-//     F: Fn(&mut Qureg<'_, N>, i32) -> Result<(), QuestError>,
-// {
-//     qubits.iter().try_for_each(|&q| gate(qureg, q))
-// }
+fn tensor_gate<'a, F, const N: u16>(
+    qureg: &mut Qureg<'a, N>,
+    gate: F,
+    qubits: &[i32],
+) -> Result<(), QuestError>
+where
+    F: for<'b> Fn(&'b mut Qureg<'a, N>, i32) -> Result<(), QuestError>,
+{
+    qubits.iter().try_for_each(|&q| gate(qureg, q))
+}
 
-// fn apply_oracle<const N: u16>(
-//     qureg: &mut Qureg<'_, N>,
-//     qubits: &[i32],
-//     sol_elem: i64,
-// ) -> Result<(), QuestError> { let sol_ctrls = &qubits .iter()
-//   .filter_map(|&q| ((sol_elem >> q) & 1 == 0).then_some(q))
-//   .collect::<Vec<_>>();
+fn apply_oracle<const N: u16>(
+    qureg: &mut Qureg<'_, N>,
+    qubits: &[i32],
+    sol_elem: i64,
+) -> Result<(), QuestError> {
+    let sol_ctrls = &qubits
+        .iter()
+        .filter_map(|&q| ((sol_elem >> q) & 1 == 0).then_some(q))
+        .collect::<Vec<_>>();
 
-//     // apply X to transform |solElem> into |111>
-//     qureg
-//         .multi_qubit_not(sol_ctrls)
-//         // effect |111> -> -|111>
-//         .and(qureg.multi_controlled_phase_flip(qubits))
-//         // apply X to transform |111> into |solElem>
-//         .and(qureg.multi_qubit_not(sol_ctrls))
-// }
+    // apply X to transform |solElem> into |111>
+    qureg
+        .multi_qubit_not(sol_ctrls)
+        // effect |111> -> -|111>
+        .and(qureg.multi_controlled_phase_flip(qubits))
+        // apply X to transform |111> into |solElem>
+        .and(qureg.multi_qubit_not(sol_ctrls))
+}
 
-// fn apply_diffuser<const N: u16>(
-//     qureg: &mut Qureg<'_, N>,
-//     qubits: &[i32],
-// ) -> Result<(), QuestError> { // apply H to transform |+> into |0>
-//   tensor_gate(qureg, Qureg::hadamard, qubits) // apply X to transform |11..1>
-//   into |00..0> .and(qureg.multi_qubit_not(qubits))?;
+fn apply_diffuser<const N: u16>(
+    qureg: &mut Qureg<'_, N>,
+    qubits: &[i32],
+) -> Result<(), QuestError> {
+    // apply H to transform |+> into |0>
+    tensor_gate(qureg, Qureg::hadamard, qubits) // apply X to transform |11..1>  into |00..0>
+        .and(qureg.multi_qubit_not(qubits))?;
 
-//     // effect |11..1> -> -|11..1>
-//     qureg.multi_controlled_phase_flip(qubits)?;
+    // effect |11..1> -> -|11..1>
+    qureg.multi_controlled_phase_flip(qubits)?;
 
-//     qureg.multi_qubit_not(qubits).and(tensor_gate(
-//         qureg,
-//         Qureg::hadamard,
-//         qubits,
-//     ))
-// }
+    qureg.multi_qubit_not(qubits).and(tensor_gate(
+        qureg,
+        Qureg::hadamard,
+        qubits,
+    ))
+}
 
-// fn main() -> Result<(), QuestError> {
-//     let env = &QuestEnv::new();
+fn main() -> Result<(), QuestError> {
+    let env = &QuestEnv::new();
 
-//     let num_reps = (PI / 4.0 * (NUM_ELEMS as Qreal).sqrt()).ceil() as usize;
-//     println!(
-//         "num_qubits: {NUM_QUBITS}, num_elems: {NUM_ELEMS}, num_reps: \
-//          {num_reps}"
-//     );
-//     // randomly choose the element for which to search
-//     let mut rng = rand::thread_rng();
-//     let sol_elem = rng.gen_range(0..NUM_ELEMS);
+    let num_reps = (PI / 4.0 * (NUM_ELEMS as Qreal).sqrt()).ceil() as usize;
+    println!(
+        "num_qubits: {NUM_QUBITS}, num_elems: {NUM_ELEMS}, num_reps: \
+         {num_reps}"
+    );
+    // randomly choose the element for which to search
+    let mut rng = rand::thread_rng();
+    let sol_elem = rng.gen_range(0..NUM_ELEMS);
 
-//     // FIXME
-//     const N: u16 = NUM_QUBITS as u16;
-//     // prepare |+>
-//     let mut qureg = Qureg::<'_, N>::try_new(env)?;
-//     qureg.init_plus_state();
-//     // use all qubits in the register
-//     let qubits = &(0..NUM_QUBITS).collect::<Vec<_>>();
+    // FIXME
+    const N: u16 = NUM_QUBITS as u16;
+    // prepare |+>
+    let mut qureg = Qureg::<'_, N>::try_new(env)?;
+    qureg.init_plus_state();
+    // use all qubits in the register
+    let qubits = &(0..NUM_QUBITS).collect::<Vec<_>>();
 
-//     // apply Grover's algorithm
-//     (0..num_reps).try_for_each(|_| {
-//         apply_oracle(&mut qureg, qubits, sol_elem)
-//             .and(apply_diffuser(&mut qureg, qubits))
-//             .and(qureg.get_prob_amp(sol_elem))
-//             .map(|prob| {
-//                 println!("prob of solution |{sol_elem}> = {:.8}", prob);
-//             })
-//     })
-// }
-
-fn main() {}
+    // apply Grover's algorithm
+    (0..num_reps).try_for_each(|_| {
+        apply_oracle(&mut qureg, qubits, sol_elem)
+            .and(apply_diffuser(&mut qureg, qubits))
+            .and(qureg.get_prob_amp(sol_elem))
+            .map(|prob| {
+                println!("prob of solution |{sol_elem}> = {:.8}", prob);
+            })
+    })
+}
